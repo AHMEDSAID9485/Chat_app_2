e13323e0c3c46fc42f552e68ce7bbb4cba4d349e
AHMEDSAID9485 <ahmedascahmed@gmail.com>
2026-06-05 21:17:06 +0300

    add model hud in login and register

--- lib/pages/Login_page.dart
+++ lib/pages/Login_page.dart
@@ -6,159 +6,178 @@ import 'package:chatapp2/widgets/Custom_Text_field_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool showSpinner = false;
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.prim_color,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Center(
-          child: Form(
-            key: formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/scholar.png'),
-                  Text(
-                    'Scholar Chat',
-                    style: TextStyle(color: AppColor.sco_color, fontSize: 28),
-                  ),
-                  SizedBox(height: 25),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Sign In',
-                        style: TextStyle(
-                          color: AppColor.sco_color,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 5),
-                  Custom_Text_field_widget(
-                    autofillHints: const [AutofillHints.email],
-                    text: 'Email',
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "this field can't be empty";
-                      }
-                      var reg = RegExp(
-                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
-                      );
-
-                      if (!reg.hasMatch(value)) {
-                        return "valid_message";
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    controller: emailController,
-                  ),
-                  SizedBox(height: 5),
-                  Custom_Text_field_widget(
-                    text: 'password',
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "this field can't be empty";
-                      }
-                      return null;
-                    },
-                    isPassword: true,
-                    controller: passwordController,
-                    keyboardType: TextInputType.visiblePassword,
-                  ),
-                  SizedBox(height: 10),
-                  CustomButtom(
-                    text: 'Sign In',
-                    onTap: () async {
-                      if (formKey.currentState!.validate()) {
-                        try {
-                          await login_method(
-                            emailController.text,
-                            passwordController.text,
-                          );
-
-                          // رسالة النجاح داخل الـ try
-                          show_snackbar(context, 'Login successful!');
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'user-not-found') {
-                            show_snackbar(
-                              context,
-                              'No user found for that email.',
-                              isError: true,
-                            );
-                          } else if (e.code == 'wrong-password') {
-                            show_snackbar(
-                              context,
-                              'Wrong password provided for that user.',
-                              isError: true,
-                            );
-                          }
-                          // الخطأ الخاص بالتحديثات الجديدة لفايربيز
-                          else if (e.code == 'invalid-credential') {
-                            show_snackbar(
-                              context,
-                              'Invalid email or password.',
-                              isError: true,
-                            );
-                          } else {
-                            show_snackbar(
-                              context,
-                              'Error: ${e.message}',
-                              isError: true,
-                            );
-                          }
-                        } catch (e) {
-                          // التقاط الأخطاء العامة
-                          show_snackbar(
-                            context,
-                            'Login failed: ${e.toString()}',
-                            isError: true,
-                          );
-                        }
-                      }
-                    },
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Don\'t have an account?',
-                        style: TextStyle(
-                          color: AppColor.sco_color,
-                          fontSize: 15,
-                        ),
-                      ),
-
-                      GestureDetector(
-                        onTap: () => Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegisterPage(),
-                          ),
-                        ),
-                        child: Text(
-                          ' Register',
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Center(
+            child: Form(
+              key: formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/scholar.png'),
+                    Text(
+                      'Scholar Chat',
+                      style: TextStyle(color: AppColor.sco_color, fontSize: 28),
+                    ),
+                    SizedBox(height: 25),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Sign In',
                           style: TextStyle(
-                            color: AppColor.third_color,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
+                            color: AppColor.sco_color,
+                            fontSize: 20,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    Custom_Text_field_widget(
+                      autofillHints: const [AutofillHints.email],
+                      text: 'Email',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "this field can't be empty";
+                        }
+                        var reg = RegExp(
+                          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                        );
+
+                        if (!reg.hasMatch(value)) {
+                          return "valid_message";
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      controller: emailController,
+                    ),
+                    SizedBox(height: 5),
+                    Custom_Text_field_widget(
+                      text: 'password',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "this field can't be empty";
+                        }
+                        return null;
+                      },
+                      isPassword: true,
+                      controller: passwordController,
+                      keyboardType: TextInputType.visiblePassword,
+                    ),
+                    SizedBox(height: 10),
+                    CustomButtom(
+                      text: 'Sign In',
+                      onTap: () async {
+                        if (formKey.currentState!.validate()) {
+                          setState(() {
+                            showSpinner = true;
+                          });
+                          try {
+                            await login_method(
+                              emailController.text,
+                              passwordController.text,
+                            );
+
+                            // رسالة النجاح داخل الـ try
+                            show_snackbar(context, 'Login successful!');
+                          } on FirebaseAuthException catch (e) {
+                            if (e.code == 'user-not-found') {
+                              show_snackbar(
+                                context,
+                                'No user found for that email.',
+                                isError: true,
+                              );
+                            } else if (e.code == 'wrong-password') {
+                              show_snackbar(
+                                context,
+                                'Wrong password provided for that user.',
+                                isError: true,
+                              );
+                            }
+                            // الخطأ الخاص بالتحديثات الجديدة لفايربيز
+                            else if (e.code == 'invalid-credential') {
+                              show_snackbar(
+                                context,
+                                'Invalid email or password.',
+                                isError: true,
+                              );
+                            } else {
+                              show_snackbar(
+                                context,
+                                'Error: ${e.message}',
+                                isError: true,
+                              );
+                            }
+                          } catch (e) {
+                            // التقاط الأخطاء العامة
+                            show_snackbar(
+                              context,
+                              'Login failed: ${e.toString()}',
+                              isError: true,
+                            );
+                          }
+                          
+                          // التعديل هنا: إيقاف التحميل بعد انتهاء المحاولة (سواء نجحت أو فشلت)
+                          setState(() {
+                            showSpinner = false;
+                          });
+                        }
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Don\'t have an account?',
+                          style: TextStyle(
+                            color: AppColor.sco_color,
+                            fontSize: 15,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () => Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterPage(),
+                            ),
+                          ),
+                          child: Text(
+                            ' Register',
+                            style: TextStyle(
+                              color: AppColor.third_color,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- lib/pages/Register_page.dart
+++ lib/pages/Register_page.dart
@@ -5,130 +5,175 @@ import 'package:chatapp2/widgets/CustomButtom.dart';
 import 'package:chatapp2/widgets/Custom_Text_field_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  bool showSpinner = false;
   final TextEditingController emailController = TextEditingController();
+
   final TextEditingController passwordController = TextEditingController();
+
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColor.prim_color,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Center(
-          child: Form(
-            key: formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/scholar.png'),
-                  Text(
-                    'Scholar Chat',
-                    style: TextStyle(color: AppColor.sco_color, fontSize: 28),
-                  ),
-                  SizedBox(height: 25),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Sign Up',
-                        style: TextStyle(color: AppColor.sco_color, fontSize: 20),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 5),
-                  Custom_Text_field_widget(
-                    validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "this field can't be empty";
-                    }
-                    var reg = RegExp(
-                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
-                    );
-
-                    if (!reg.hasMatch(value)) {
-                      return "valid_message";
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  autofillHints: const [AutofillHints.email],
-                    text: 'Email',
-                    controller: emailController,
-                  ),
-                  SizedBox(height: 5),
-                  Custom_Text_field_widget(
-                    text: 'password',
-                     validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "this field can't be empty";
-                    }
-                    return null;
-                  },
-                    isPassword: true,
-                    controller: passwordController,
-                  ),
-                  SizedBox(height: 10),
-                  CustomButtom(
-                    text: 'Register',
-                    onTap: () async {
-                if (formKey.currentState!.validate()) {
-  try {
-    // 1. محاولة إنشاء الحساب
-    await Register_method(emailController.text, passwordController.text);
-    
-    // 2. رسالة النجاح 
-    show_snackbar(context, 'Registration successful!');
-    
-    // يمكنك إضافة كود الانتقال للصفحة الرئيسية هنا
-    
-  } on FirebaseAuthException catch (e) {
-    // 3. التقاط أخطاء فايربيز الخاصة بالتسجيل
-    if (e.code == 'weak-password') {
-      show_snackbar(context, 'The password provided is too weak.', isError: true);
-    } 
-    else if (e.code == 'email-already-in-use') {
-      show_snackbar(context, 'The account already exists for that email.', isError: true);
-    } 
-    else {
-      // لالتقاط أي خطأ آخر من فايربيز
-      show_snackbar(context, 'Error: ${e.message}', isError: true);
-    }
-  } catch (e) {
-    // 4. التقاط الأخطاء العامة غير المتوقعة
-    show_snackbar(context, 'Registration failed: ${e.toString()}', isError: true);
-  }
-}}
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Already have an account?',
-                        style: TextStyle(color: AppColor.sco_color, fontSize: 15),
-                      ),
-            
-                      GestureDetector(
-                        onTap: () => Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(builder: (context) => LoginPage()),
-                        ),
-                        child: Text(
-                          ' Sign In',
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
+        backgroundColor: AppColor.prim_color,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Center(
+            child: Form(
+              key: formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/scholar.png'),
+                    Text(
+                      'Scholar Chat',
+                      style: TextStyle(color: AppColor.sco_color, fontSize: 28),
+                    ),
+                    SizedBox(height: 25),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Sign Up',
                           style: TextStyle(
-                            color: AppColor.third_color,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
+                            color: AppColor.sco_color,
+                            fontSize: 20,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    Custom_Text_field_widget(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "this field can't be empty";
+                        }
+                        var reg = RegExp(
+                          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                        );
+
+                        if (!reg.hasMatch(value)) {
+                          return "valid_message";
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      autofillHints: const [AutofillHints.email],
+                      text: 'Email',
+                      controller: emailController,
+                    ),
+                    SizedBox(height: 5),
+                    Custom_Text_field_widget(
+                      text: 'password',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "this field can't be empty";
+                        }
+                        return null;
+                      },
+                      isPassword: true,
+                      controller: passwordController,
+                    ),
+                    SizedBox(height: 10),
+                    CustomButtom(
+                      text: 'Register',
+                      onTap: () async {
+                        if (formKey.currentState!.validate()) {
+                          setState(() {
+                            showSpinner = true;
+                          });
+                          try {
+                            // 1. محاولة إنشاء الحساب
+                            await Register_method(
+                              emailController.text,
+                              passwordController.text,
+                            );
+
+                            // 2. رسالة النجاح
+                            show_snackbar(context, 'Registration successful!');
+
+                            // يمكنك إضافة كود الانتقال للصفحة الرئيسية هنا
+                          } on FirebaseAuthException catch (e) {
+                            // 3. التقاط أخطاء فايربيز الخاصة بالتسجيل
+                            if (e.code == 'weak-password') {
+                              show_snackbar(
+                                context,
+                                'The password provided is too weak.',
+                                isError: true,
+                              );
+                            } else if (e.code == 'email-already-in-use') {
+                              show_snackbar(
+                                context,
+                                'The account already exists for that email.',
+                                isError: true,
+                              );
+                            } else {
+                              // لالتقاط أي خطأ آخر من فايربيز
+                              show_snackbar(
+                                context,
+                                'Error: ${e.message}',
+                                isError: true,
+                              );
+                            }
+                          } catch (e) {
+                            // 4. التقاط الأخطاء العامة غير المتوقعة
+                            show_snackbar(
+                              context,
+                              'Registration failed: ${e.toString()}',
+                              isError: true,
+                            );
+                          }
+                        }
+                        setState(() {
+                        showSpinner = false;
+                      });
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already have an account?',
+                          style: TextStyle(
+                            color: AppColor.sco_color,
+                            fontSize: 15,
+                          ),
+                        ),
+
+                        GestureDetector(
+                          onTap: () => Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ),
+                          ),
+                          child: Text(
+                            ' Sign In',
+                            style: TextStyle(
+                              color: AppColor.third_color,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -136,5 +181,4 @@ class RegisterPage extends StatelessWidget {
       ),
     );
   }
-
 }
